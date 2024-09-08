8a88aca75fdd7f003f687165c996b05b9da53d89
Elhem Rebhi <elhemrebhi@ELHEM.local>
2024-09-08 22:34:09 +0200

    add endpoints

--- routes/relationshipRoutes.js
+++ routes/relationshipRoutes.js
@@ -10,5 +10,5 @@ router.post('/assign-partner', apiKeyMiddleware, assignPartner);
 // Route to update the sliders in a relationship
 // Optionally protected with API key middleware
 router.post('/update-sliders', apiKeyMiddleware, updateSliders);
-router.post('/getRelationships', apiKeyMiddleware, getRelationships);
+router.get('/getRelationships', apiKeyMiddleware, getRelationships);
 module.exports = router;
